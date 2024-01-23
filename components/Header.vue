65a6b59858edc4a7ad11de0c270f51d2ce901e21
Tony <qkrdkwl9090@naver.com>
2024-01-23 15:54:54 +0900

    feat(main): default page, layout, header

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="w-screen | shrink-0 fixed z-10 | bg-white">
+    <section class="py-4 flex items-center justify-between px-6 xl:px-8">
+      <div class="flex items-center">
+        <NuxtLink
+          id="logo"
+          class="font-bold text-xl flex items-center mr-20"
+          to="/"
+        >
+          <picture>
+            <source srcset="/logo.webp" type="image/webp" />
+            <img src="/logo.png" alt="logo" class="w-[50px]" />
+          </picture>
+          Coin Info
+        </NuxtLink>
+        <nav>
+          <ul class="flex gap-10">
+            <li><NuxtLink to="/">가상자산</NuxtLink></li>
+            <li><NuxtLink to="/">거래소</NuxtLink></li>
+            <li><NuxtLink to="/">커뮤니티</NuxtLink></li>
+            <li><NuxtLink to="/">서비스</NuxtLink></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main__container overflow-x-hidden shrink-0">
+    <Header />
+    <main><slot /></main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+main {
+  min-height: 100vh;
+  min-height: 100dvh;
+}
+</style>
