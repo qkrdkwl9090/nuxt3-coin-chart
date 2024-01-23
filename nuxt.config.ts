b240950d93f08b1e80c2eb945fad25fdadc9548f
Tony <qkrdkwl9090@naver.com>
2024-01-23 13:48:30 +0900

    feat(styles): set tailwind css, scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/nuxt3-coin-chart/",
   },
+  css: ["~/assets/styles/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
