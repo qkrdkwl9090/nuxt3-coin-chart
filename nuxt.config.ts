0dbd1ea03414dce71789c184e5999b4102b6674d
Tony <qkrdkwl9090@naver.com>
2024-01-23 19:35:15 +0900

    feat(theme): set primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,22 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/nuxt3-coin-chart/",
   },
-  modules: ["nuxt-lodash"],
-  css: ["@/assets/styles/font.scss", "@/assets/styles/main.scss"],
+  modules: ["nuxt-lodash", "nuxt-primevue"],
+  primevue: {
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+    options: {
+      ripple: true,
+    },
+    components: {
+      include: ["Button"],
+    },
+  },
+  css: [
+    "@/assets/styles/font.scss",
+    "primevue/resources/themes/lara-light-green/theme.css",
+    "@/assets/styles/main.scss",
+    "primeicons/primeicons.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
